5823c348dc79e179e8e20185926dbba16e924b0c
rakirs <srikarmylavarapu@gmail.com>
2022-11-20 11:19:37 -0600

    updating with some helpers

---
+++ helpers/RandomChar.py
@@ -0,0 +1,21 @@
+import random
+
+# for i in range(10000):
+#     print(random.randint(1,3))
+
+
+options = ['rock', 'paper', 'scissors']
+
+# for i in range(10000):
+#     print(random.choice(options))
+
+
+mapOptions = {
+    'r': "rock",
+    'p': "paper",
+    's': "scissors"
+}
+# print(mapOptions['p'])
+
+# for i in range(10000):
+#     print(random.choice(list(mapOption)))
