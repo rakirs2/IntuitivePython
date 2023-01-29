ee35cae2697b3aec3f4cefd2f6c1b3f048786535
EJmak <118122540+EJmak@users.noreply.github.com>
2023-01-29 10:03:14 -0600

    Create HW_1.py

---
+++ EJ/Submissions/HW_1.py
@@ -0,0 +1,15 @@
+#Favorite Food
+FavoriteFood="Chicken"
+print(f"{FavoriteFood}")
+FavoriteFood="ChickenWings"
+print(f"{FavoriteFood}")
+#Int vs. Floats
+print(float(3.2591+2.12093))
+print(float(2.12093+3.2591))
+#Family Members Age Calculator
+Mom=37
+Maddison=6
+Zuri=2
+Ethan=14
+print(Mom+Maddison+Zuri+Ethan)
+print(Mom+Maddison+Zuri)#exluding myself
