475a77cd0ff8c3d39d82cb42bfcf0aea2e65d7ea
EJmak <118122540+EJmak@users.noreply.github.com>
2023-03-06 13:23:52 -0600

    Create C3P_3.py

---
+++ EJ/Submissions/C3P_3.py
@@ -0,0 +1,8 @@
+#3-3 Your Own List - Program used to print multiple statements of things I want it to say(Transport)
+Transport = ["car",'motorcycle','bus','train','airplane']
+message = f"Although I have many ways to get places that I like to use, my absolute favorite is most likely to be by {Transport[0].title()}."
+print(message)
+message = f"I will say, however even though {Transport[0]} is one of the favorite methods of transport, for longer distances I would much rather use a {Transport[1]}, because it's much faster, and I also am fairly sure that if I ever got a motorcycle it would be probably a Harley Davidson"
+print(message)
+message = f"I will say though as much as a {Transport[0]}, motorcycle, and plane would be fine by me I don't get much of a chance to use {Transport[3]}, because there isn't much reason to."
+print(message)
