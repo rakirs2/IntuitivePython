8b02ea57b518e2f03e375494557a913b531de1a4
EJmak <118122540+EJmak@users.noreply.github.com>
2023-03-06 13:09:01 -0600

    Create C4P_8.py

---
+++ EJ/Submissions/C4P_8.py
@@ -0,0 +1,7 @@
+#4-8: Cubes, use a for loop to print cubes of integers from 1 to 10.
+cubes = []
+for cubes in range(1,11):
+    cubes = range(1,11) ** 3 
+    cubes.append(cubes)
+    print(cubes)
+#Farthest I got.(Not completed)
