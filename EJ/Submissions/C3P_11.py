53f5eb1728329def8ceb97173cf80c6b1d7bfb00
EJmak <118122540+EJmak@users.noreply.github.com>
2023-03-12 10:57:13 -0500

    Create C3P_11.py (#47)

---
+++ EJ/Submissions/C3P_11.py
@@ -0,0 +1,6 @@
+#3-11 Intentional Error: Purposefully create an index error(fix it later, of course.)
+Index = []
+Index.append('Tartarus')
+print(Index[0])
+#Total of around 2-3 hours I think.
+#Fixed version, it's really easy to make an index
