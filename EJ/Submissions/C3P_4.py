8f7796e0fd2547fe27cbdfbec669c8832bba00b6
EJmak <118122540+EJmak@users.noreply.github.com>
2023-03-19 15:28:15 -0500

    Create C3P_4.py (#43)

---
+++ EJ/Submissions/C3P_4.py
@@ -0,0 +1,4 @@
+#3-4 Guest List - Invite a list of people to a dinner.
+Invitations = ['Zackry','Allen','Jackson']
+Invite = f"About dinner today, {Invitations[2].title()} do you want to go with {Invitations[1].title()}, {Invitations[0].title()} and me?"
+print(Invite)
