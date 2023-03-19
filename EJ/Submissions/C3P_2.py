a35227f680fb41b1a3acceeb78279be7a495fd40
EJmak <118122540+EJmak@users.noreply.github.com>
2023-03-19 14:54:08 -0500

    Create C3P_2.py (#46)

---
+++ EJ/Submissions/C3P_2.py
@@ -0,0 +1,5 @@
+#3-2 Greetings: List I already used, but with a message specific to each of them.
+names = ['Kailyn','Willow','London']
+print(f"Good morning, {names[0].title()}, how did you sleep?")
+print(f"How have you been {names[1].title()}?")
+print(f"Hey {names[2].title()}, I gotta show you this!")
