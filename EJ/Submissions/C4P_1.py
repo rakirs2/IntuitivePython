18390ca9c9ed61804b20b8e3a5fc2c06c7fa36a4
EJmak <118122540+EJmak@users.noreply.github.com>
2023-03-19 15:29:02 -0500

    Create C4P_1.py (#30)

---
+++ EJ/Submissions/C4P_1.py
@@ -0,0 +1,5 @@
+#4-1: List of pizzas, loop a statement, then add one at the end.
+pizza = ['bacon','sausage','pepperoni']
+for pizza in pizza:
+    print(f"I think {pizza.title()} Pizza is really good for days you have no food\n")
+print("I really like these kinds of pizza.")
