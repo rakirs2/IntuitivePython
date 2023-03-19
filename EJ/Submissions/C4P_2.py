f57700cce724a69206146bffe982d7d97e8c2328
EJmak <118122540+EJmak@users.noreply.github.com>
2023-03-19 15:30:44 -0500

    Create C4P_2.py (#31)

---
+++ EJ/Submissions/C4P_2.py
@@ -0,0 +1,5 @@
+#4-2: Animals 3 different animals that have a common charictaristc, then print some statements about it.
+Pets = ['dog','cat','hamster']
+for Pets in Pets:
+    print(f"A {Pets} would make a really good companion, especially if you live alone!\n")
+    print(f"I think a {Pets} would live to be great companions, however unfortunately it doesn't live forever.\n")
