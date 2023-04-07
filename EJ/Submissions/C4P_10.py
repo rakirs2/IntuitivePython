8f569f427fab7635089b08dbfdabe38f6569d51b
EJmak <118122540+EJmak@users.noreply.github.com>
2023-04-07 17:14:18 -0500

    Create C4P_10.py
    I forgot how to do the git thingy and add my own stuff :(

---
+++ EJ/Submissions/C4P_10.py
@@ -0,0 +1,9 @@
+#4-10 Slices: Using one of the programs you wrote in this chapter, add several lines to the end of the program that do some functions
+#I can't really find one I can use(make a new list)
+sample = ['Very Easy','Easy','Medium','Hard','Insane','Demon','Extreme Demon']
+print("The first three items in the list are:")
+print(sample[0:3])
+print("Three items from the middle of the list are:")
+print(sample[2:5])
+print("The last three items in the list are")
+print(sample[4:7])
