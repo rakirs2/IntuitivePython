59cdf06229e507d4ec416e9a9c549a1d15f5f4d0
EJmak <118122540+EJmak@users.noreply.github.com>
2023-04-07 17:15:51 -0500

    Create C5P_2.py

---
+++ EJ/Submissions/C5P_2.py
@@ -0,0 +1,16 @@
+#5-2 More Conditional Tests: Perform more comparisons using the "True" or "False" value, having atleast 1 be true, and 1 be false.
+number = 20
+print(number > 21)
+print(number < 21)
+letter = 'A'
+print("Is the letter == A? I predict True.")
+print(letter == 'A')
+
+print("Is the letter == A? I predict False")
+print(letter.lower() == 'A')
+
+print(number >= 21)
+print(number <= 21)
+print(number > 21 and number >= 21)
+print(number <= 21 and number < 21)
+#2 hour cap reached.
