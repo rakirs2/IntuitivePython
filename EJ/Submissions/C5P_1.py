e07883608070788de4a2ad4006825c52bfa0a5ac
EJmak <118122540+EJmak@users.noreply.github.com>
2023-04-07 17:15:25 -0500

    Create C5P_1.py

---
+++ EJ/Submissions/C5P_1.py
@@ -0,0 +1,22 @@
+#5-1 Conditional Tests: Write a  series of conditional tests.
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+print("\nIs car == 'toyota'? I predict False.")
+print(car == 'toyota')
+
+print("\nIs car == 'bmw'? I predict False.")
+print(car == 'bmw')
+
+print("\nIs car == 'jeep'? I predict False.")
+print(car == 'jeep')
+
+print("\nIs car == 'hummer'? I predict False.")
+print(car == 'hummer')
+
+print("\nIs car == 'crysler'? I predict False.")
+print(car == 'crysler')
