fa8a6baa78ec8c003e954c10a452e69484d6384c
EJmak <118122540+EJmak@users.noreply.github.com>
2023-04-15 19:31:11 -0500

    Create C5P_1.py

---
+++ EJ/Submissions/PythonCrashCourse/C5P_1.py
@@ -0,0 +1,34 @@
+#5-1 Conditional Tests: Write a  series of conditional tests.
+car = 'subaru'
+roblox = 'sandbox'
+Horrific = 'difficulty'
+
+print("\nThe difficulty of Tower of the Roof's Pique is == 'Horrific', that's true according to most victors of it.")
+print(Horrific == 'difficulty')
+
+print("\nRoblox in my opinion is a == 'sandbox', due to the the fact that you can literally create any game you want, limited to the 32bit engine that it runs.")
+print(roblox == 'sandbox')
+
+print("\nOne of the most well known luxury cars is == 'bmw', you literally can't go around the world without hearing of seeing one.")
+print(car == 'bmw')
+
+print("\nI doubt I'll ever buy an == 'audi', I don't hear the best about them, and I hear they're very costly to maintain.")
+print(car == 'audi')
+
+print("\nI think an awesome car would be a == 'Toyota', however the main issue with them is the cost, I'm not super sure I can look past the cost as easily as I would wanna say I can.")
+print(car == 'toyota')
+
+print("\nI think one of the most ideal cars would be something like a == 'subaru' since they're one of the best sounding cars that I've personally heard of, and I hear that they're really safe, too.")
+print(car == 'subaru')
+
+print("\nI think one of the best sandbox games inside of = 'roblox' is probably Life In Prison. Although I don't like Roblox, this game doesn't irritate me as much as most others do.")
+print(roblox == 'sandbox')
+
+print("\nI would love to beat a Horrific tower, however they seem == 'unfeasable' to me because they're so far out of my skill range for obbies.")
+print(Horrific == 'unfeasable')
+
+print("\nI think that = 'subaru' also makes really good looking cars, compared to what I usually see nowadays.")
+print(car == 'subaru')
+
+print("\nProbably the best Horrific tower in my opinion is one of the most difficult ones, Tower of The Roof's Pique, it uses a really unique design, compared to most other towers that go in ascending order, this one goes left to right.")
+print(Horrific == "Tower of the Roof's pique")
